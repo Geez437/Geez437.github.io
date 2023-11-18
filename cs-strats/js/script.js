173119334072f043846df45cd0571e8649402da7
Maxi Buthenhoff <maxi.buthenhoff@gmail.com>
2023-11-18 13:06:15 +0100

    added videos

--- cs-strats/js/script.js
+++ cs-strats/js/script.js
@@ -1,2 +1,13 @@
-// JavaScript for interactive features
-// (Currently empty or include basic functions as needed)
+function openPopup(videoId) {
+    var popup = document.getElementById(videoId);
+    if (popup) {
+        popup.style.display = 'flex'; // Use flex to center the video
+    }
+}
+
+// Close popup when clicking outside of the video
+window.onclick = function(event) {
+    if (event.target.classList.contains('popup')) {
+        event.target.style.display = 'none';
+    }
+}
